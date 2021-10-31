d09a3e4a9846efb7ffac8e5626adbe71593adeb4
Isaac Yeon <yissixflags@gmail.com>
2021-10-31 21:00:10 +0900

    Add files via upload

---
+++ 2775.py
@@ -0,0 +1,10 @@
+T = int(input())
+
+for i in range(T):
+    k = int(input())
+    n = int(input())
+    k0 = [x for x in range(1, n+1)]
+    for i in range(k):
+        for j in range(1, n):
+            k0[j] += k0[j-1]
+    print(k0[-1])
