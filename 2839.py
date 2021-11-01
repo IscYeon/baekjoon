70e3fa09b63169faaf8aa8e6abb1d092f3b4f58e
Isaac Yeon <yissixflags@gmail.com>
2021-11-01 21:27:59 +0900

    Add files via upload

---
+++ 2839.py
@@ -0,0 +1,18 @@
+N = int(input())
+bag = -1
+
+if N%5 == 0:
+    bag = N//5
+elif N%5 == 1:
+    bag = N//5 - 1
+    bag += 2
+elif N%5 == 4 and N != 4:
+    bag = N//5 - 1
+    bag += 3
+elif N%5 == 3:
+    bag = N//5 + 1
+elif N%5 == 2 and N != 7:
+    bag = N//5 - 2
+    bag += 4
+
+print(bag)
